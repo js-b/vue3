09335bb2d9c92bbb37714221ca59ef4183a839d9
Jaxan <whitedragon99@gk.ru>
2021-06-02 21:35:27 +0500

    Дополнения

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 Vue.createApp({
-    data: ()=> ({
-        myHtml: '<h1> Vue3 App</h1>',
-        title : 'Главная',
-        person: {
-            firstName: 'Jaxan',
-            lastName:'Sabitov',
-            age: 21
-        }
-    })
-}).mount('#app')
+  data: () => ({
+    myHtml: "<h1> Vue3 App</h1>",
+    title: "Главная",
+    person: {
+      firstName: "Jaxan",
+      lastName: "Sabitov",
+      age: 21,
+    },
+    items: [1, 2],
+  }),
+  methods: {
+    addItem() {
+        this.items.unshift(this.$refs.myInput.value)
+        this.$refs.myInput.value = ''
+    },
+    remove(i){
+        this.items.splice(i,1)
+    },
+    log(item){
+        console.log('Log item:', item);
+    }
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter((i) => i % 2 === 0);
+    },
+  },
+}).mount("#app");
